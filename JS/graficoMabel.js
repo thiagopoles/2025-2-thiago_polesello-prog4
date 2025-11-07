672a35d1bbe86df3606029401059ef956e152709
thiagopoles <polesellothiago14@gmail.com>
2025-11-07 16:41:08 -0300

    Commit

--- JS/graficoMabel.js
+++ JS/graficoMabel.js
@@ -1,5 +1,4 @@
-/*gráfico mabel*/
-
+/* Gráfico Mabel*/
 var options = {
     series: [{
     name: "Temperatura Externa",
@@ -81,50 +80,3 @@ grid: {
 
 var chart = new ApexCharts(document.querySelector("#chart"), options);
 chart.render();
-
-
-/* gráfico ptqa 
-
-var options = {
-        series: [{
-        name: "STOCK ABC",
-        data: series.monthDataSeries1.prices
-    }],
-        chart: {
-        type: 'area',
-        height: 350,
-        zoom: {
-        enabled: false
-        }
-    },
-    dataLabels: {
-        enabled: false
-    },
-    stroke: {
-        curve: 'straight'
-    },
-
-    title: {
-        text: 'Fundamental Analysis of Stocks',
-        align: 'left'
-    },
-    subtitle: {
-        text: 'Price Movements',
-        align: 'left'
-    },
-    labels: series.monthDataSeries1.dates,
-    xaxis: {
-        type: 'datetime',
-    },
-    yaxis: {
-        opposite: true
-    },
-    legend: {
-        horizontalAlign: 'left'
-    }
-    };
-
-    var chart = new ApexCharts(document.querySelector("#chart"), options);
-    chart.render();*/
-
-
