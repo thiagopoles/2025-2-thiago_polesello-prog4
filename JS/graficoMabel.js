ceebb6384b5ca0ff1984a7bfc879f864f34bac8a
isabelytrevisan <isabely.ot25@aluno.ifsc.edu.br>
2025-11-14 16:43:20 -0300

    Update graficoMabel.js

--- JS/graficoMabel.js
+++ JS/graficoMabel.js
@@ -1,47 +1,106 @@
-fetch("consultasSQL.sql")
+fetch("mabel_dados.php")
     .then(response => response.json())
     .then(data => {
 
-        /* Gráfico Mabel */
-        var options = {
-            series: [
-                {
-                    name: "Temperatura Externa",
-                    data: data.te
-                },
-                {
-                    name: "Temperatura Interna",
-                    data: data.ti
-                },
-                {
-                    name: "Temperatura Do Ninho",
-                    data: data.tn
+var options = {
+    series: [{
+            name: "Temperatura Externa",
+            data: data.te
+        },
+        {
+            name: "Temperatura Interna",
+            data: data.ti
+        },
+        {
+            name: 'Temperatura Do Ninho',
+            data: data.ninho
+        },
+        {
+            name: 'Umidade Externa',
+            data: data.he // Dados de exemplo para a umidade externa
+        },
+        {
+            name: 'Umidade Interna',
+            data: data.hi // Dados de exemplo para a umidade interna
+        }
+    ],
+    chart: {
+        height: 350,
+        type: 'line',
+        zoom: {
+            enabled: false
+        },
+    },
+    dataLabels: {
+        enabled: false
+    },
+    stroke: {
+        width: [5, 7, 5, 5, 5], // Ajustei para todas as linhas
+        curve: 'straight', // Linha reta sem curvatura
+        dashArray: [0, 0, 0, 0, 0] // Removi os pontilhados, agora todas são linhas contínuas
+    },
+    title: {
+        text: '',
+        align: 'left'
+    },
+    legend: {
+        tooltipHoverFormatter: function(val, opts) {
+            return val + ' - <strong>' + opts.w.globals.series[opts.seriesIndex][opts.dataPointIndex] + '</strong>';
+        }
+    },
+    markers: {
+        size: 0,
+        hover: {
+            sizeOffset: 6
+        }
+    },
+    xaxis: {
+        categories: ['01 Jan', '02 Jan', '03 Jan', '04 Jan', '05 Jan', '06 Jan', '07 Jan', '08 Jan', '09 Jan', '10 Jan', '11 Jan', '12 Jan']
+    },
+    tooltip: {
+        y: [{
+                title: {
+                    formatter: function(val) {
+                        return val + " (mins)";
+                    }
                 }
-            ],
-            chart: {
-                height: 350,
-                type: 'line',
-                zoom: { enabled: false }
             },
-            dataLabels: { enabled: false },
-            stroke: {
-                width: [5, 7, 5],
-                curve: 'straight',
-                dashArray: [0, 8, 5]
+            {
+                title: {
+                    formatter: function(val) {
+                        return val + " per session";
+                    }
+                }
             },
-            title: {
-                text: 'Estatísticas Do Ninho',
-                align: 'left'
+            {
+                title: {
+                    formatter: function(val) {
+                        return val;
+                    }
+                }
             },
-            markers: { size: 0 },
-            xaxis: {
-                categories: data.categorias
+            {
+                title: {
+                    formatter: function(val) {
+                        return val + " %"; // Para umidade
+                    }
+                }
             },
-            grid: {
-                borderColor: '#f1f1f1',
+            {
+                title: {
+                    formatter: function(val) {
+                        return val + " %"; // Para umidade
+                    }
+                }
             }
-        };
+        ]
+    },
+    grid: {
+        borderColor: '#f1f1f1',
+    }
+};
 
-        var chart = new ApexCharts(document.querySelector("#chart"), options);
-        chart.render();
-    });
+var chart = new ApexCharts(document.querySelector("#chart"), options);
+chart.render();
+
+});
