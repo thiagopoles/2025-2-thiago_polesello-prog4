c52db9e50ee18bf045177a8398d070ff62e5c69e
isabelytrevisan <isabely.ot25@aluno.ifsc.edu.br>
2025-11-24 10:35:44 -0300

    commit

--- PHP/mysqlConnection.php
+++ PHP/mysqlConnection.php
@@ -1,17 +1,23 @@
 <?php
-error_reporting(E_ALL); 
+error_reporting(E_ALL);
 ini_set("display_errors", 1);
 
 $servername = "localhost";
-$username = "root";        // padrão do XAMPP
-$password = "";            // sem senha
-$dbname = "mabel_ptqa_heitor_isabely_thiago"; 
+$username = "root"; 
+$password = "";
+$dbname = "mabel_ptqa_heitor_isabely_thiago";
 
 try {
-  $conecta = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8", $username, $password);
-  $conecta->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  //echo "Conexão bem-sucedida!";
+    $conecta = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8", $username, $password);
+    $conecta->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) {
-  echo "Connection failed: " . $e->getMessage();
+
+    // Retorna JSON válido, sem HTML
+    header('Content-Type: application/json; charset=utf-8');
+    echo json_encode([
+        "erro" => "Falha na conexão com o banco",
+        "detalhes" => $e->getMessage()
+    ]);
+    exit;
 }
 ?>
