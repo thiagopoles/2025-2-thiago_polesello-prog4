e660e881ffd45f42467744101669626579d88e3a
thiagopoles <polesellothiago14@gmail.com>
2025-11-28 17:21:51 -0300

    commit

--- JS/graficosMABEL/loaderMABEL.js
+++ JS/graficosMABEL/loaderMABEL.js
@@ -1,7 +1,4 @@
-let chart = null;
-
 document.getElementById("btnPesquisar").addEventListener("click", () => {
-
     const grafico = document.getElementById("selecionarGrafico").value;
     const inicio = document.getElementById("dataInicial").value;
     const fim = document.getElementById("dataFinal").value;
@@ -11,10 +8,31 @@ document.getElementById("btnPesquisar").addEventListener("click", () => {
         return;
     }
 
-    if (chart) chart.destroy();
-
-    const script = document.createElement("script");
-    script.src = `localhost/2025-2-thiago_polesello-prog4/JS/graficosMABEL/${grafico}.js?inicio=${inicio}&fim=${fim}&v=` + Date.now();
-
-    document.body.appendChild(script);
+    // Passando os parâmetros para o gráfico
+    carregarGrafico(grafico, inicio, fim);
 });
+
+// Função que irá passar os dados para o gráfico
+function carregarGrafico(grafico, inicio, fim) {
+    // Checa qual gráfico deve ser carregado
+    switch (grafico) {
+        case 'ti':
+            carregarTemperaturaInterna(inicio, fim);
+            break;
+        case 'te':
+            carregarTemperaturaExterna(inicio, fim);
+            break;
+        case 'hi':
+            carregarUmidadeInterna(inicio, fim);
+            break;
+        case 'he':
+            carregarUmidadeExterna(inicio, fim);
+            break;
+        case 'ninho':
+            carregarTemperaturaNinho(inicio, fim);
+            break;
+        default:
+            alert("Selecione um gráfico!");
+            break;
+    }
+}
--- JS/graficosMABEL/ti.js
+++ JS/graficosMABEL/ti.js
@@ -1,20 +1,17 @@
-const url = new URL(document.currentScript.src);
-const inicio = url.searchParams.get("inicio");
-const fim = url.searchParams.get("fim");
-
-fetch(`localhost/2025-2-thiago_polesello-prog4/PHP/consultasMABEL/ti.php?inicio=${inicio}&fim=${fim}`)
-.then(r => r.json())
-.then(dados => {
-
-    chart = new Chart(document.getElementById("grafico"), {
-        type: "line",
-        data: {
-            labels: dados.map(x => x.datahora),
-            datasets: [{
-                label: "Temperatura Interna (°C)",
-                data: dados.map(x => x.ti)
-            }]
-        }
+function carregarTemperaturaInterna(inicio, fim) {
+    fetch(`../PHP/consultasMABEL/ti.php?inicio=${inicio}&fim=${fim}`)
+    .then(r => r.json())
+    .then(dados => {
+        chart = new Chart(document.getElementById("grafico"), {
+            type: "line",
+            data: {
+                labels: dados.map(x => x.datahora),
+                datasets: [{
+                    label: "Temperatura Interna (°C)",
+                    data: dados.map(x => x.ti),
+                    borderWidth: 2
+                }]
+            }
+        });
     });
-
-});
+}
